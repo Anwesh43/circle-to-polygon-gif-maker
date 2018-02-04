5046cba20a3abb080f54d20354f8976c263b1435
Anwesh43 <anweshthecool0@gmail.com>
2018-02-04 20:02:01 +0530

    adding test files

---
+++ test.js
@@ -0,0 +1,4 @@
+const circleToPolygonGifMaker  = require('.')
+circleToPolygonGifMaker(4,300,300,"#0D47A1",'circle_to_square.gif')
+circleToPolygonGifMaker(3,300,300,"#0D47A1",'circle_to_triangle.gif')
+circleToPolygonGifMaker(5,300,300,"#0D47A1",'circle_to_polygon.gif')
